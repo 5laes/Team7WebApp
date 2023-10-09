ee2fd33701081f3b90e5fcc8282711207303d3ff
DanielDarwiche <danielinti1@hotmail.com>
2023-10-09 11:50:40 +0200

    classes

---
+++ Team7WebApp/Models/Abscence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team7WebAPI.Models
+{
+    public class Abscence
+    {
+        [Key]
+        int id { get; set; }
+        int typeID { get; set; }
+        int days { get; set; }
+        int daysRequested { get; set; }
+        int personID { get; set; }
+        bool pending { get; set; }
+        bool approved { get; set; }
+        DateTime leaveStart { get; set; }
+        DateTime leaveEnd { get; set; }
+    }
+}
---
+++ Team7WebApp/Models/AbscenceType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Team7WebAPI.Models
+{
+    public class AbscenceType
+    {
+        [Key]
+        int id { get; set; }
+        string typeName { get; set; }
+        int days { get; set; }
+    }
+}
