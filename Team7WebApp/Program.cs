bc51ea33ba0abb3e3bdc050f6a93674d57915843
elli82 <114058163+elli82@users.noreply.github.com>
2023-10-17 15:42:23 +0200

    Added Get-all persons(employees)

--- Team7WebApp/Program.cs
+++ Team7WebApp/Program.cs
@@ -60,6 +60,8 @@ namespace Team7WebApp
 
             app.UseAuthorization();
 
+            app.UseCors("CORSPolicy");
+
 
             //----------------------------------------------------------------------------------------------------------------------
             //ENDPOINTS FOR PERSON START
