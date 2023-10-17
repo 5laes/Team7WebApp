3006296429687618b73a48afafb6e2b7fef300f0
DanielDarwiche <danielinti1@hotmail.com>
2023-10-17 14:04:54 +0200

    loginpage on react. visual studio = corspolicy

--- react-webapp/src/Components/AdminPage.jsx
+++ react-webapp/src/Components/AdminPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
+let names=["Pedro", "Jose", "Gabriel","Pedro"];
+
+names.map((namn)=>{
+  return "Mr. "+namn;
+  //above adds to the variables(REPLACES value)
+  // return <h1>{namn}</h1>  //returns h1
+});
+
+names.filter((namNet)=>{
+  return namNet !== "Pedro"; //excludes PedroS
+})
+
+
 export default function AdminPage() {
   return (
     <div>
       <p>admin</p>
       <p>admin</p>
-      <p>admin</p>
+      <button onClick={()=>{console.log('hello from anonymous function')}}>consolebutton</button>
     </div>
   );
 }
