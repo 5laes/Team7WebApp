c12be06bc00d75bb56c1e12d126330a04f2b114d
Claes Lundin <claeslundin@pm.me>
2023-10-20 11:13:09 +0200

    auth added

---
+++ Team7WebApp/Models/DTOs/PersonLoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Team7WebApp.Models.DTOs
+{
+	public class PersonLoginDTO
+	{
+		[Required]
+		public string email { get; set; }
+		[Required]
+		public string password { get; set; }
+	}
+}
